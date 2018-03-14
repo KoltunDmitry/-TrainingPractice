9f59652c484b2c717dbeddca2d48e60bbdfd7912
Dmitry Koltun <koltundima999@gmail.com>
2018-03-15 02:24:02 +0300

    made a DOM  & improved HTML, CSS

---
+++ Site/js/function.js
@@ -0,0 +1,263 @@
+"use strict";
+ var functionPost = (function () {
+
+    var photoPosts =  [
+         {
+             id: '1',
+             description: 'Навальный 20!82',
+             createdAt: new Date('2018-09-23T23:00:00'),
+             author: 'Иванов Иван',
+             photoLink: 'http://www.vladtime.ru/uploads/posts/2018-01/1515326200_navalnyi-o-alishere-usmanove-mitingah-i-kirille-serebrennikove.jpg',
+             hashTag: ["#20!8"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '2',
+             description: 'I like to use instagram',
+             createdAt: new Date('2018-10-23T17:00:00'),
+             author: 'Васильев Василий',
+             photoLink: 'https://9to5mac.files.wordpress.com/2017/08/instagram.jpg?quality=82&strip=all&w=1500',
+             hashTag: ["#InstagramRulit", "#instagram"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '3',
+             description: 'Домрачева выиграла золото!!!',
+             createdAt: new Date('2018-04-23T15:00:00'),
+             author: 'Дмитриев Дмитрий',
+             photoLink: 'http://www.vladtime.ru/uploads/posts/2018-01/1515326200_navalnyi-o-alishere-usmanove-mitingah-i-kirille-serebrennikove.jpg',
+             hashTag: ["#Domracheva", "#Gold"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '4',
+             description: 'Алексеев попал на Евровидение',
+             createdAt: new Date('2018-05-23T12:00:00'),
+             author: 'Алексеев Алексей',
+             photoLink: 'https://www.eurovoix.com/wp-content/uploads/2017/12/alekseev.jpg',
+             hashTag: ["#Alekseev"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         /*{
+             id: '5',
+             description: 'Yeaaahh',
+             createdAt: new Date('2018-06-23T10:00:00'),
+             author: 'Александров Александр',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '6',
+             description: '++++!!!',
+             createdAt: new Date('2018-06-23T12:00:00'),
+             author: 'Иванов Иван',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '7',
+             description: '+++++++++++++----------------------',
+             createdAt: new Date('2018-06-23T17:00:00'),
+             author: 'Васильев Василий',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },*/
+         /*{
+             id: '8',
+             description: 'BelarusChina',
+             createdAt: new Date('2018-12-23T15:00:00'),
+             author: 'Дмитриев Дмитрий',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '10',
+             description: 'Алексеев попал в колонию',
+             createdAt: new Date('2018-08-01T12:00:00'),
+             author: 'Алексеев Алексей',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '9',
+             description: 'У меня ДР',
+             createdAt: new Date('2018-08-12T10:00:00'),
+             author: 'Колтун Дмитрий',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         },
+         {
+             id: '11',
+             description: 'Женская',
+             createdAt: new Date('2018-09-02T23:00:00'),
+             author: 'Иванов Иван',
+             photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+             hashTag: ["#hashtag"],
+             like: ["Dima", "IVAN"],
+             isDelete: false
+         }*/
+     ];
+
+     function compareDate(a, b) {
+         return b.createdAt - a.createdAt;
+     }
+
+     function unique(arr) {
+         var obj = {};
+         for(let i = 0; i < arr.length;i++){
+             var str = arr[i];
+             obj[str] = true;
+         }
+         return Object.keys(obj);
+     }
+     function getAllAuthor(){
+        var name = [];
+        photoPosts.forEach(function(item){
+
+             if (!item.isDelete)
+                 name.push(item.author);
+         })
+        return unique(name);
+ }
+     function getAllHashtags() {
+         var hashtags = [];
+         photoPosts.forEach(function (itemPost) {
+            if(!itemPost.isDelete) {
+                itemPost.hashTag.forEach(function (itemHashtag) {
+                        hashtags.push(itemHashtag)
+                    }
+                )
+            }
+         });
+         return unique(hashtags);
+     }
+     function getPhotoPosts(skip, top, filterConfig) {
+         skip = skip || 0;
+         top = top || 0;
+         var min = top;
+         if(min > photoPosts.length){
+             min = photoPosts.length;
+         }
+         var newArr = [];
+         if (!filterConfig) {
+             return photoPosts.sort(compareDate).slice(skip,min);
+         }
+         else {
+             var result = photoPosts;
+             if (filterConfig.author) {
+                 result = result.filter(function (post) {
+                     return post.author === filterConfig.author;
+                 })
+             }
+             if (filterConfig.createdAt) {
+                 result = result.filter(function (post) {
+                     return post.createdAt === filterConfig.createdAt;
+                 })
+             }
+             if (filterConfig.hashTag) {
+                 result = result.filter(function (post) {
+                     for (let hashTag of post.hashTag) {
+                         for (let hashTagSearch of [].concat(filterConfig.hashTag)) {
+                             if (hashTag === hashTagSearch) {
+                                 return true;
+                             }
+                         }
+                     }
+                 });
+             }
+             return result.sort(compareDate).slice(skip, min);
+         }
+     }
+
+     function getPhotoPost(id) {
+         var found = photoPosts.find(function (element) {
+             return element.id === id;
+         });
+         return found;
+     }
+
+     function validatePhotoPost(post,isForAddPost) {
+         var isValidate = true;
+         if (typeof post.id !== "string" || typeof post.description !== "string"
+             || typeof post.author !== "string" || typeof post.photoLink !== "string"
+             || !(post.createdAt instanceof Date) || !(post.hashTag instanceof Array)) {
+             return false
+         }
+         photoPosts.forEach(function (item) {
+             if (item.id === post.id) {
+                 return false;
+             }
+         });
+         if (post.description.length === 0 || post.description.length >= 200) {
+             return false;
+         }
+         if(post.photoLink.length === 0){
+             return false;
+         }
+         if (!post.createdAt || post.createdAt.toString() === "Invalid Date") {
+             return false;
+         }
+         if (post.author.length === 0 || !post.author) {
+             return false;
+         }
+         return true;
+     }
+
+     function addPhotoPost(post) {
+         if (validatePhotoPost(post)) {
+             post.isDelete = false;
+             photoPosts.push(post);
+             return true;
+         }
+         return false;
+     }
+
+
+     function removePhotoPostLabeled(id) {
+         var found = photoPosts.findIndex(i => i.id === id);
+         photoPosts[found].isDelete = true;
+     }
+
+     function editPhotoPost(id, object) {
+         var found = photoPosts.findIndex(i => i.id === id);
+         if(found!== -1){
+             if(object.description && object.description){
+                 photoPosts[found].description = object.description;
+             }
+             if(object.hashTag && object.hashTag.length!==0){
+                 photoPosts[found].hashTag = [];
+                 photoPosts[found].hashTag.concat(object.hashTag);
+             }
+             if(object.photoLink && object.photoLink!==0) {
+                 photoPosts[found].photoLink = object.photoLink;
+             }
+         }
+     }
+     console.log(getPhotoPosts(0,5,{author:"Дмитриев Дмитрий"}));
+     return {
+         getPhotoPosts,
+         getPhotoPost,
+         validatePhotoPost,
+         addPhotoPost,
+         editPhotoPost,
+         removePhotoPostLabeled,
+         getAllAuthor,
+         getAllHashtags
+     }
+ }());
--- Site/js/main.js
+++ Site/js/main.js
@@ -1,274 +1,191 @@
-"use strict";
-(function () {
+'use strict';
+var MyModule =(function() {
 
-    var photoPosts = [
-        {
-            id: '1',
-            description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-            createdAt: new Date('2018-02-23T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag1", "#hashtag2"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '2',
-            description: '----------------------',
-            createdAt: new Date('2018-03-23T17:00:00'),
-            author: 'Васильев Василий',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
+    var user = "Иванов Иван";
+    setNickname();
+    setPhotoProfile();
+    function showPhotoPost(photoPost) {
+        var post = document.createElement("div");
+        post.className = "post";
+        var headPost = document.createElement("div");
+        headPost.className = "headPost";
+        if(user === photoPost.author) {
+            var buttonHead = document.createElement("div");
+            buttonHead.className = "buttonHead";
+            var buttonDelete = document.createElement("button");
+            var buttonEdit = document.createElement("button");
+            buttonDelete.innerHTML = "<img src = \"image/waste.png\" alt = \"delete\" class=\"deletePost\">";
+            buttonEdit.innerHTML = "<img src = \"image/edit.jpg\" alt = \"edit\" class=\"editPost\">";
+            buttonHead.appendChild(buttonDelete);
+            buttonHead.appendChild(buttonEdit);
+            headPost.appendChild(buttonHead);
+        }
+        var imgAvatar = document.createElement("img");
+        imgAvatar.setAttribute("src","image/profile.png");
+        imgAvatar.className = "roundAvatar";
+        headPost.appendChild(imgAvatar);
+        var blockNameDate = document.createElement("div");
+        blockNameDate.className = "blockNameDate";
+        var nicknamePost = document.createElement("div");
+        nicknamePost.className = "nicknamePost";
+        nicknamePost.innerHTML = photoPost.author;
+        blockNameDate.appendChild(nicknamePost);
+        var datePhoto = document.createElement("div");
+        datePhoto.className = "datePhoto";
+        datePhoto.innerHTML = formatDate(photoPost.createdAt);
+        blockNameDate.appendChild(datePhoto);
+        headPost.appendChild(blockNameDate);
+
+        //nice
+        var text = document.createElement("div");
+        text.className = "text";
+        text.innerHTML = photoPost.description;
+        var photoPlace = document.createElement("div");
+        photoPlace.className = "photoPlace";
+        var photo = document.createElement("img");
+        photo.setAttribute("src",photoPost.photoLink);
+        photo.className = "photo";
+        photoPlace.appendChild(photo);
+        //nice
+        var likeAndHashtags = document.createElement("div");
+        likeAndHashtags.className = "likeAndHashtags";
+
+        var a = document.createElement("a");
+        a.setAttribute("href", "#");
+        a.innerHTML = "<img src = \"image/whiteheart.png\" alt = \"like\" class=\"like\">";
+        likeAndHashtags.appendChild(a);
+        var line = document.createElement("img");
+        line.className = "lineLikeAndHashtags";
+        line.src="image/line.png";
+        likeAndHashtags.appendChild(line);
+        var hashtagInPost = document.createElement("span");
+        hashtagInPost.className = "hashtagInPost";
+        var hashTags = "";
+        photoPost.hashTag.forEach(function (item) {
+            hashTags+= item + " ";
+        });
+        hashtagInPost.innerHTML = hashTags;
+        likeAndHashtags.appendChild(hashtagInPost);
+        post.appendChild(headPost);
+        post.appendChild(text);
+        post.appendChild(photoPlace);
+        post.appendChild(likeAndHashtags);
+        var posts = document.getElementById("posts");
+        posts.appendChild(post);
+        addFilterAuthor();
+        addFilterHashtags();
+    }
+    function formatDate(date) {
+
+        var dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+
+        var mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+
+        var yy = date.getFullYear() % 100;
+        if (yy < 10) yy = '0' + yy;
+
+        return dd + '.' + mm + '.' + yy;
+    }
+
+    function addFilterAuthor(){
+        var listAuthor = document.getElementById("listAuthor");
+        var nameAuthors = functionPost.getAllAuthor();
+        var formDatalist = "";
+        nameAuthors.forEach(function (item) {
+            formDatalist += "<option>" + item + "</option>";
+        });
+        listAuthor.innerHTML = formDatalist;
+    }
+    function addFilterHashtags(){
+        var listHashtags = document.getElementById("suggestions");
+        var nameHashtags = functionPost.getAllHashtags();
+        var formDatalist = "";
+        nameHashtags.forEach(function (item) {
+            formDatalist += "<option>" + item + "</option>";
+        });
+        listHashtags.innerHTML = formDatalist;
+    }
+    function setNickname() {
+        if (user) {
+            var nickname = document.getElementById("nicknameHeader");
+            nickname.innerHTML = "<h3>" + user + "</h3>";
+        }
+    }
+    function setPhotoProfile(){
+        if(user){
+            var photoProfile = document.getElementById("containerProfile");
+            var a = document.createElement("a");
+            a.setAttribute("href", "#");
+            a.innerHTML = "<img src=\"image/arrow.png\" alt=\"\" class=\"arrow-icon\">"
+                +"<img src=\"image/profile.png\" alt=\"\" id=\"profile\">";
+            photoProfile.appendChild(a);
+        }
+    }
+    function showPosts(skip, top, filterConfig) {
+        //document.getElementsByClassName("posts")[0].innerHTML = "";
+        update();
+        var photoPosts = functionPost.getPhotoPosts(skip, top, filterConfig);
+        for (var i = 0; i < photoPosts.length; i++) {
+            if(!photoPosts[i].isDelete)
+            showPhotoPost(photoPosts[i]);
+        }
+    }
+    function addPhotoPost(post) {
+        functionPost.addPhotoPost(post);
+        showPosts(0,10);
+    }
+
+    function removePhotoPostLabeled(id) {
+        functionPost.removePhotoPostLabeled(id);
+        showPosts(0,10);
+    }
+
+    function editPost(id,post) {
+        functionPost.editPhotoPost(id, post);
+        showPosts(0,10);
+    }
+    function update(){
+        var posts = document.getElementById("posts");
+        posts.innerHTML = "";
+    }
+       /* setNickname();
+        setPhotoProfile();
+        showPosts(0,10);
+        removePhotoPostLabeled("1");
+        editPost("3",{
             id: '3',
             description: 'China',
             createdAt: new Date('2018-04-23T15:00:00'),
             author: 'Дмитриев Дмитрий',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
+            photoLink: 'http://www.vladtime.ru/uploads/posts/2018-01/1515326200_navalnyi-o-alishere-usmanove-mitingah-i-kirille-serebrennikove.jpg',
+            hashTag: ["#some"],
             like: ["Dima", "IVAN"],
             isDelete: false
-        },
-        {
-            id: '4',
-            description: 'Алексеев попал на Евровидение',
-            createdAt: new Date('2018-05-23T12:00:00'),
-            author: 'Алексеев Алексей',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '5',
-            description: 'Yeaaahh',
-            createdAt: new Date('2018-06-23T10:00:00'),
-            author: 'Александров Александр',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '6',
-            description: '++++!!!',
-            createdAt: new Date('2018-06-23T12:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '7',
-            description: '+++++++++++++----------------------',
-            createdAt: new Date('2018-06-23T17:00:00'),
-            author: 'Васильев Василий',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '8',
-            description: 'BelarusChina',
-            createdAt: new Date('2018-12-23T15:00:00'),
-            author: 'Дмитриев Дмитрий',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '10',
-            description: 'Алексеев попал в колонию',
-            createdAt: new Date('2018-08-01T12:00:00'),
-            author: 'Алексеев Алексей',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '9',
-            description: 'У меня ДР',
-            createdAt: new Date('2018-08-12T10:00:00'),
-            author: 'Колтун Дмитрий',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        },
-        {
-            id: '11',
-            description: 'Женская',
-            createdAt: new Date('2018-09-02T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-            hashTag: ["#hashtag"],
-            like: ["Dima", "IVAN"],
-            isDelete: false
-        }
-    ];
+        })*/
+       showPosts(0,5);
+       editPost("1", {description:"кого-то не допустили до выборов"});
+       removePhotoPostLabeled("3");
+       addPhotoPost({
+           id: '10',
+           description: 'Домрачева выиграла золото!!!',
+           createdAt: new Date('2018-04-23T15:00:00'),
+           author: 'Дмитриев Дмитрий',
+           photoLink: 'http://www.vladtime.ru/uploads/posts/2018-01/1515326200_navalnyi-o-alishere-usmanove-mitingah-i-kirille-serebrennikove.jpg',
+           hashTag: ["#Domracheva", "#Gold"],
+           like: ["Dima", "IVAN"],
+           isDelete: false
+       });
 
-    function compareDate(a, b) {
-        return a.createdAt - b.createdAt;
-    }
-
-    function print(a, b) {
-        for (var i = a; i < b; i++) {
-            if (photoPosts[i]) {
-                console.log(photoPosts[i].id + " " + photoPosts[i].description + " " + photoPosts[i].createdAt + " " +
-                    photoPosts[i].author + " " + photoPosts[i].photoLink + " " + photoPosts[i].hashTag + " " + photoPosts[i].like + "\n")
-            }
-        }
-    }
-
-    function getPhotoPosts(skip, top, filterConfig) {
-        skip = skip || 0;
-        top = top || 0;
-        var newArr = [];
-        if (!filterConfig) {
-            return photoPosts.slice(skip, skip + top).length
-        }
-        else {
-            var result = photoPosts;
-            if (filterConfig.author) {
-                result = result.filter(function (post) {
-                    return post.author === filterConfig.author;
-                })
-            }
-            if (filterConfig.createdAt) {
-                result = result.filter(function (post) {
-                    return post.createdAt === filterConfig.createdAt;
-                })
-            }
-            if (filterConfig.hashTag) {
-                result = result.filter(function (post) {
-                    for (let hashTag of post.hashTag) {
-                        for (let jtem of [].concat(filterConfig.hashTag)) {
-                            if (hashTag === jtem) {
-                                return true;
-                            }
-                        }
-                    }
-                });
-            }
-            return result.sort(compareDate).slice(skip, top);
-        }
-    }
-
-    function getPhotoPost(id) {
-        var found = photoPosts.find(function (element) {
-            return element.id === id;
-        })
-        return found;
-    }
-
-    function validatePhotoPost(post) {
-        var isValidate = true;
-        if (typeof post.id !== "string" || typeof post.description !== "string"
-            || typeof post.author !== "string" || typeof post.photoLink !== "string"
-            || !(post.createdAt instanceof Date) || !(post.hashTag instanceof Array)) {
-            return false
-        }
-        for (var i = 0; i < photoPosts.length; i++) {
-            if (photoPosts[i].id === post.id) {
-                return false;
-            }
-        }
-        if (post.description.length === 0 || post.description.length >= 200) {
-            return false;
-        }
-        if (!post.createdAt || post.createdAt.toString() === "Invalid Date") {
-            return false;
-        }
-        if (post.author.length === 0 || !post.author) {
-            return false;
-        }
-        return true;
-    }
-
-    function addPhotoPost(post) {
-        if (validatePhotoPost(post)) {
-            console.log(post)
-            if (!post.isDelete) {
-                post.isDelete = false;
-            }
-            photoPosts[photoPosts.length] = post;
-            return true
-        }
-        return false
-    }
-
-    function removePhotoPost(id) {
-        var found = photoPosts.findIndex(i => i.id === id);
-        photoPosts.splice(found, 1)
-    }
-
-    function removePhotoPostLabeled(id) {
-        var found = photoPosts.findIndex(i => i.id === id);
-        photoPosts[found].isDelete = true;
-    }
-
-    function editPhotoPost(id, object) {
-        for (var i = 0; i < photoPosts.length; i++) {
-            if (photoPosts[i].id === id) {
-                var temp = Object.assign({}, photoPosts[i]);
-                if (object.photoLink) {
-                    if (object.photoLink.length !== 0) {
-                        temp.photoLink = object.photoLink
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                if (object.description) {
-                    if (object.description.length === 0 || object.description.length >= 200) {
-                        return false;
-                    }
-                    else {
-                        temp.description = object.description
-                    }
-                }
-                if (object.hashTag) {
-                    if (object.hashTag.length > 0 || object.hashTag instanceof Array) {
-                        temp.hashTag = object.hashTag
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                photoPosts[i] = temp
-                return true;
-            }
-        }
-    }
-    /*var post =[{
-        id : '15',
-        description: 'Женская',
-        createdAt: new Date("1/1/2018"),
-        author: 'Dima',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTag: ["#hashtag"]
-    },{
-        id : '16',
-        description: 'asfasf',
-        createdAt: new Date("1/1/2018"),
-        author: 'Dima',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTag: "#fdsf",
-        isDelete:false
-    }]*/
     return {
-        getPhotoPost: getPhotoPost,
-        getPhotoPosts:getPhotoPosts,
-        validatePhotoPost:validatePhotoPost,
-        addPhotoPost:addPhotoPost,
-        editPhotoPost:editPhotoPost,
-        removePhotoPostLabeled:removePhotoPostLabeled,
-    }
+        showPhotoPost,
+        showPosts,
+        editPost,
+        removePhotoPostLabeled,
+        addPhotoPost,
+        update,
+        addFilterHashtags,
+        addFilterAuthor
+    };
 })();
